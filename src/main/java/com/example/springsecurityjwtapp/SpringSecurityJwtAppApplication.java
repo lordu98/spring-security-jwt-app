fd788183c0ec547307afa38a220cacb5ec0a6667
lordu98 <u.makhmudov211326@gmail.com>
2021-07-09 16:47:00 +0500

    Initial commit

---
+++ src/main/java/com/example/springsecurityjwtapp/SpringSecurityJwtAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityjwtapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityJwtAppApplication.class, args);
+    }
+
+}
